1fad923c00af03045d4e2ff03213971f5c32ccd9
Benjamin Reinhard <>
2026-06-12 12:58:02 +0200

    Add inputs

--- src/renderer.c
+++ src/renderer.c
@@ -8,14 +8,14 @@ void render_cur_board(State s) {
 	for (int i = 0; i < s.m; i++) {
 		for (int j = 0; j < s.n; j++) {
 			switch (s.cur_board[i][j]) {
+			case D:
+				printc(D);
+				break;
 			case O:
 				printf("%s ", "\u223F");
 				break;
 			case X: case M:
 				printf("%s ", "\u25CF");
-				break;
-			case D:
-				printc(D);
 				break;
 			}
 		}
